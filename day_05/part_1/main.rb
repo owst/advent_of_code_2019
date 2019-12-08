38d7855d1082d10cae70ac26d464a91733cd1b33
Owen Stephens <owen@owenstephens.co.uk>
2019-12-08 10:40:44 +0000

    Fixup Day 05 part 1

--- day_05/part_1/main.rb
+++ day_05/part_1/main.rb
@@ -5,10 +5,10 @@ end
 def resolve_op(op_num)
   case op_num
   when 99 then [:break, 0]
-  when 1 then [:+, 2]
-  when 2 then [:*, 2]
-  when 3 then [:read, 0]
-  when 4 then [:write, 0]
+  when 1 then [:+, %i[r r w]]
+  when 2 then [:*, %i[r r w]]
+  when 3 then [:read, %i[w]]
+  when 4 then [:write, %i[r]]
   else
     raise "Unexpected op_num: #{op_num}"
   end
@@ -16,18 +16,22 @@ end
 
 def resolve_instruction(input)
   n = [input.length, 2].min
-  op, arg_count = resolve_op(input.slice!(-n, n).to_i)
-  arg_modes = input.chars.reverse.map(&:to_i).each_with_index.map { |c, i|
+  op, arg_modes = resolve_op(input.slice!(-n, n).to_i)
+  address_modes = input.chars.reverse.map(&:to_i).each_with_index.map { |c, i|
     [i, c.zero? ? :position : :immediate]
   }.to_h
 
-  [arg_modes, op, arg_count]
+  [address_modes, op, arg_modes]
 end
 
-def resolve_arguments(state, address_modes, args)
-  args.each_with_index.map do |arg, i|
-    address_modes.fetch(i, :position) == :position ? state.fetch(arg) : arg
-  end
+def resolve_arguments(state, address_modes, pc, arg_modes)
+  (1..arg_modes.size).map { |arg_num|
+    state.fetch(pc + arg_num)
+  }.each_with_index.map { |arg, i|
+    dereference = arg_modes[i] == :r && address_modes.fetch(i, :position) == :position
+
+    dereference ? state.fetch(arg) : arg
+  }
 end
 
 def run_program(instructions)
@@ -36,25 +40,23 @@ def run_program(instructions)
   pc = 0
 
   loop do
-    address_modes, op, arg_count = resolve_instruction(state.fetch(pc).to_s)
+    address_modes, op, arg_modes = resolve_instruction(state.fetch(pc).to_s)
 
     break if op == :break
 
-    unresolved_args = (1..arg_count).map { |arg_num| state.fetch(pc + arg_num) }
-    target_address = state.fetch(pc + arg_count + 1)
-    args = resolve_arguments(state, address_modes, unresolved_args)
+    *args, target = resolve_arguments(state, address_modes, pc, arg_modes)
 
     case op
     when :+, :*
-      state[target_address] = args[0].send(op, args[1])
+      state[target] = args[0].send(op, args[1])
     when :read
       puts "Enter number:"
-      state[target_address] = $stdin.readline.to_i
+      state[target] = $stdin.readline.to_i
     when :write
-      puts state[target_address]
+      puts target
     end
 
-    pc += arg_count + 1 + 1 # 1 for instruction, 1 for target address
+    pc += arg_modes.size + 1
   end
 
   state
