a196cc01b95e8f9be03f4a6b4af1d3ee4f931536
Owen Stephens <owen@owenstephens.co.uk>
2019-12-16 21:51:44 +0000

    Day 12 part 2 start

---
+++ day_12/part_2/main.rb
@@ -0,0 +1,114 @@
+require "set"
+
+Vector = Struct.new(:x, :y, :z) do
+  def self.zero
+    new(0, 0, 0)
+  end
+
+  def +(other)
+    self.class.new(x + other.x, y + other.y, z + other.z)
+  end
+
+  def abs_max(other)
+    self.class.new(*to_a.map(&:abs).zip(other.to_a.map(&:abs)).map(&:max))
+  end
+
+  def pretty(max)
+    digit_count = ->(val) { Math.log10(val).floor + 1 }
+
+    # +1 to account for (optional) leading "-" in overal width
+    c = ->(max_val) { "% #{max_val.zero? ? 1 : digit_count.call(max_val) + 1}d" }
+
+    format("<x=#{c.call(max.x)}, y=#{c.call(max.y)}, z=#{c.call(max.z)}>", x, y, z)
+  end
+end
+
+Moon = Struct.new(:position, :velocity) do
+  def self.parse(line)
+    values = line.gsub(/[<>\s]+/, '').split(",").map do |assign|
+      var, value = assign.split("=")
+
+      [var, value.to_i]
+    end.to_h
+
+    new(Vector.new(values.fetch("x"), values.fetch("y"), values.fetch("z")), Vector.zero)
+  end
+
+  def potential_energy
+    vector_energy(position)
+  end
+
+  def kinetic_energy
+    vector_energy(velocity)
+  end
+
+  def energy
+    potential_energy * kinetic_energy
+  end
+
+  def pretty(max_pos, max_vel)
+    "pos=#{position.pretty(max_pos)}, vel=#{velocity.pretty(max_vel)}"
+  end
+
+  def apply_velocity
+    self.class.new(position + velocity, velocity)
+  end
+
+  def apply_gravity(other)
+    pos = position
+    opos = other.position
+
+    self.class.new(
+      pos,
+      velocity + Vector.new(opos.x <=> pos.x, opos.y <=> pos.y, opos.z <=> pos.z)
+    )
+  end
+
+  private def vector_energy(vector)
+    vector.to_a.map(&:abs).sum
+  end
+end
+
+def read_input(input = nil)
+  input ||= $stdin.read
+
+  input.split("\n").map { |line| Moon.parse(line) }
+end
+
+def print(moons)
+  maxes = moons.reduce([Vector.zero, Vector.zero]) do |(max_pos, max_vel), moon|
+    [moon.position.abs_max(max_pos), moon.velocity.abs_max(max_vel)]
+  end
+
+  moons.each { |moon| puts moon.pretty(*maxes) }
+end
+
+(0..5).each do |i|
+  (0..5).each do |j|
+    state = read_input(<<~STR)
+      <x=0, y=1, z=#{i}>
+      <x=0, y=1, z=#{j}>
+      <x=0, y=1, z=3>
+      <x=0, y=1, z=4>
+    STR
+    seen = Set.new([])
+    steps = 0
+
+    loop do
+      # puts "After #{steps} steps"
+      # print(state)
+
+      break if seen.include?(state)
+
+      seen << state
+
+      state = state.map do |moon|
+        (state - [moon]).reduce(moon, :apply_gravity).apply_velocity
+      end
+
+      steps += 1
+    end
+
+    puts "Seen again after #{steps} steps for #{i}/#{j}"
+  end
+end
